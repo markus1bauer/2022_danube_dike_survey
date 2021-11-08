f3da8ad5e4c69fe827dddcee308b56baa688a36a
markus1bauer <markus1.bauer@tum.de>
2021-11-08 11:20:51 +0100

    tbi_d_pres modelled

--- R/model_betaDiversity_nmds.R
+++ R/model_betaDiversity_nmds.R
@@ -27,7 +27,7 @@ sites <- read_csv("data_processed_sites.csv", col_names = T, na = c("na", "NA", 
                       plot = "f",
                       exposition = "f"
                     )) %>%
-  select(id, plot, block, locationAbb, surveyYear, exposition, vegetationCov, targetCov, graminoidCovratio, speciesRichness, targetRichness, shannon, eveness, accumulatedCov, syn_total, syn_trend, syn_detrend) %>%
+  #select(id, plot, block, locationAbb, surveyYear, exposition, vegetationCov, targetCov, graminoidCovratio, speciesRichness, targetRichness, shannon, eveness, accumulatedCov, syn_total, syn_trend, syn_detrend) %>%
   mutate(surveyYearF = as_factor(surveyYear)) %>%
   filter(accumulatedCov > 0)
 
@@ -52,7 +52,7 @@ species <- read_csv("data_processed_species.csv", col_names = T, na = c("na", "N
 
 ### 1 NMDY #####################################################################################
 
-### Claculate ###
+### Calculate ###
 set.seed(1)
 (ordi <- metaMDS(species, dist = "bray", binary = F,
                  try = 99, previous.best = T, na.rm = T))
@@ -72,11 +72,17 @@ points(ordi, display = "sites", cex = gof * 300)
 ### 2 Environmental factors #####################################################################################
 
 #### a Vectors ----------------------------------------------------------------------------------------
-(ef_vector1 <- envfit(ordi ~  vegetationCov + graminoidCovratio + targetRichness + targetCov + speciesRichness + eveness + shannon + syn_total + syn_trend + syn_detrend, 
-              data = sites, permu = 999, na.rm = T))
+(ef_vector1 <- envfit(ordi ~  vegetationCov + graminoidCovratio + targetRichness + targetCov + 
+                        speciesRichness + eveness + shannon + syn_total.x + syn_trend.x + syn_detrend.x +
+                        log_varrat.x + log_varrat_t3.x, 
+              data = sites, 
+              permu = 999, 
+              na.rm = T))
 plot(ordi, type = "n"); plot(ef_vector1, add = T, p. = .99)
-(ef_vector2 <- envfit(ordi ~  graminoidCovratio + speciesRichness + eveness, 
-                      data = sites, permu = 999, na.rm = T))
+(ef_vector2 <- envfit(ordi ~  graminoidCovratio + speciesRichness + eveness + log_varrat_t3.x, 
+                      data = sites, 
+                      permu = 999, 
+                      na.rm = T))
 plot(ordi, type = "n"); plot(ef_vector2, add = T, p. = .99)
 
 #### b Factors ----------------------------------------------------------------------------------------
@@ -84,4 +90,3 @@ plot(ordi, type = "n"); plot(ef_vector2, add = T, p. = .99)
               data = sites, permu = 999, na.rm = T))
 plot(ordi, type = "n"); ordiellipse(ordi, sites$surveyYearF, kind = "sd", draw = "lines", label = T)
 plot(ordi, type = "n"); ordiellipse(ordi, sites$exposition, kind = "sd", draw = "lines", label = T)
-
---
+++ R/model_betadiversity_tbi_d_abundance.R
@@ -0,0 +1,269 @@
+# Calculate TBI ####
+# Markus Bauer
+# Citation: Bauer M, Huber J, Kollmann J (202x)
+
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# A Preparation ################################################################################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+### Packages ###
+library(here)
+library(tidyverse)
+library(ggbeeswarm)
+library(adespatial)
+library(lme4)  
+library(DHARMa)
+library(AICcmodavg)
+library(emmeans)
+
+### Start ###
+rm(list = ls())
+setwd(here("data/processed"))
+
+### Load data ###
+tbi <- read_csv("data_processed_tbi.csv", col_names = T, na = c("na", "NA"), col_types = 
+                    cols(
+                      .default = "?",
+                      id = "f",
+                      locationAbb = "f",
+                      block = "f",
+                      plot = "c",
+                      locationYear = "f",
+                      exposition = "f",
+                      side = "f",
+                      surveyYearF = "f",
+                      constructionYearF = "f",
+                      comparison = "f"
+                    )) %>%
+  #filter(vegetationCov > 0) %>%
+  filter(comparison %in% c("1718", "1819", "1921") & presabu == "presence") %>%
+  mutate(plotAge = as_factor(plotAge),
+         plotAge = fct_collapse(plotAge, "12.5" = c("11", "14")),
+         comparison = factor(comparison),
+         exposition = factor(exposition)) %>%
+  rename(y = D)
+
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# B Statistics ################################################################################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+### 1 Data exploration #####################################################################################
+
+#### a Graphs ---------------------------------------------------------------------------------------------
+#main
+ggplot(tbi, aes(x = comparison, y = y)) + 
+  geom_boxplot() +
+  geom_quasirandom()
+ggplot(tbi, aes(x = exposition, y = y)) + 
+  geom_boxplot() +
+  geom_quasirandom()
+ggplot(tbi, aes(x = PC1soil, y = y)) + 
+  geom_point() +
+  geom_smooth(method = "lm")
+ggplot(tbi, aes(x = plotAge, y = y)) + 
+  geom_boxplot() +
+  geom_quasirandom()
+ggplot(tbi, aes(x = side, y = y)) + 
+  geom_boxplot() +
+  geom_quasirandom()
+#2way
+ggplot(tbi, aes(x = exposition, y = y, color = comparison)) + 
+  geom_boxplot() +
+  geom_quasirandom()
+ggplot(tbi, aes(x = riverkm, y = y, color = comparison)) + 
+  geom_point() +
+  geom_smooth(method = "lm")
+ggplot(tbi, aes(x = distanceRiver, y = y, color = comparison)) + 
+  geom_point() +
+  geom_smooth(method = "lm")
+ggplot(tbi, aes(x = as.numeric(plotAge), y = y, color = comparison)) + 
+  geom_point() +
+  geom_smooth(method = "lm")
+ggplot(tbi, aes(x = PC1soil, y = y, color = comparison)) + 
+  geom_point() +
+  geom_smooth(method = "lm")
+ggplot(tbi, aes(x = PC2soil, y = y, color = comparison)) + 
+  geom_point() +
+  geom_smooth(method = "lm")
+ggplot(tbi, aes(x = locationYear, y = y)) + 
+  geom_boxplot()
+ggplot(tbi, aes(x = as.numeric(plotAge), y = y, color = exposition)) + 
+  geom_point() +
+  geom_smooth(method = "lm")
+ggplot(tbi, aes(x = riverkm, y = y, color = plotAge)) + 
+  geom_point() +
+  geom_smooth(method = "lm")
+ggplot(tbi, aes(x = PC1soil, y = y, color = plotAge)) + 
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+#3way
+ggplot(tbi, aes(x = riverkm, y = y, color = comparison)) + 
+  geom_point() +
+  geom_smooth(method = "lm") +
+  facet_wrap(~exposition)
+ggplot(tbi, aes(x = as.numeric(plotAge), y = y, color = comparison)) + 
+  geom_point() +
+  geom_smooth(method = "lm") +
+  facet_wrap(~exposition)
+ggplot(tbi, aes(x = PC1soil, y = y, color = comparison)) + 
+  geom_point() +
+  geom_smooth(method = "lm") +
+  facet_wrap(~exposition)
+ggplot(tbi, aes(x = PC2soil, y = y, color = comparison)) + 
+  geom_point() +
+  geom_smooth(method = "lm") +
+  facet_wrap(~exposition)
+
+#### b Outliers, zero-inflation, transformations? -----------------------------------------------------
+dotchart((tbi$y), groups = factor(tbi$exposition), main = "Cleveland dotplot")
+tbi %>% count(locationYear)
+boxplot(tbi$y);#identify(rep(1, length(etbi$rgr13)), etbi$rgr13, labels = c(etbi$n))
+plot(table((tbi$y)), type = "h", xlab = "Observed values", ylab = "Frequency")
+ggplot(tbi, aes(y)) + geom_density()
+ggplot(tbi, aes(sqrt(y))) + geom_density()
+ggplot(tbi, aes(PC2soil)) + geom_density()
+ggplot(tbi, aes(sqrt(PC2soil))) + geom_density()
+
+
+### 2 Model building ################################################################################
+
+#### a models ----------------------------------------------------------------------------------------
+#random structure
+m1a <- lmer(y ~ 1 + (1|plot) + (1|locationYear), tbi, REML = F)
+VarCorr(m1a) 
+m1b <- lmer(y ~ 1 + (1|plot), tbi, REML = F)
+VarCorr(m1b)
+m1c <- lmer(y ~ 1 + (1|block/plot), tbi, REML = F)
+VarCorr(m1c);isSingular(m1c)
+#fixed effects
+m1 <- lmer(sqrt(y) ~ (comparison + exposition + PC1soil + PC2soil + PC3soil + side + plotAge)^2 + comparison:exposition:PC1soil + locationYear +
+             (1|plot), 
+           REML = F,
+           data = tbi)
+isSingular(m1);simulateResiduals(m1, plot = T)
+m2 <- lmer(sqrt(y) ~ comparison + (exposition * PC1soil) + PC2soil + PC3soil + side + plotAge + locationYear + 
+             (1|plot), 
+           REML = F,
+           data = tbi)
+isSingular(m2);simulateResiduals(m2, plot = T)
+m3 <- lmer(sqrt(y) ~ comparison * (exposition + PC2soil) + PC1soil + PC3soil + side + plotAge + locationYear +
+             (1|plot), 
+           REML = F,
+           data = tbi)
+isSingular(m3);simulateResiduals(m3, plot = T)
+m4 <- lmer(sqrt(y) ~ comparison + exposition + PC1soil + PC2soil + PC3soil + side + plotAge + locationYear + 
+             (1|plot), 
+           REML = F,
+           data = tbi)
+isSingular(m4);simulateResiduals(m4, plot = T)
+m5 <- lmer(sqrt(y) ~ comparison * exposition * PC1soil + PC2soil + PC3soil + side + plotAge + locationYear +
+             (1|plot),
+           REML = F,
+           data = tbi)
+isSingular(m5);simulateResiduals(m5, plot = T)
+m6 <- lmer(sqrt(y) ~ comparison * exposition + PC1soil + PC2soil + PC3soil + side + plotAge + locationYear +
+             (1|plot),
+           REML = F, 
+           data = tbi)
+isSingular(m6);simulateResiduals(m6, plot = T)
+
+#### b comparison -----------------------------------------------------------------------------------------
+aictab(cand.set = list("m1" = m1, "m2" = m2, "m3" = m3, "m4" = m4, "m5" = m5, "m6" = m6))
+rm(m1a, m1b, m1c, m2, m3, m4, m5)
+
+#### c model check -----------------------------------------------------------------------------------------
+simulationOutput <- simulateResiduals(m4, plot = F)
+plotResiduals(simulationOutput$scaledResiduals, tbi$locationYear)
+plotResiduals(simulationOutput$scaledResiduals, tbi$block)
+plotResiduals(simulationOutput$scaledResiduals, tbi$plot)
+plotResiduals(simulationOutput$scaledResiduals, tbi$plotAge)
+plotResiduals(simulationOutput$scaledResiduals, tbi$comparison)
+plotResiduals(simulationOutput$scaledResiduals, tbi$exposition)
+plotResiduals(simulationOutput$scaledResiduals, tbi$side)
+plotResiduals(simulationOutput$scaledResiduals, tbi$PC1soil)
+plotResiduals(simulationOutput$scaledResiduals, tbi$PC2soil)
+plotResiduals(simulationOutput$scaledResiduals, tbi$PC3soil)
+plotResiduals(simulationOutput$scaledResiduals, tbi$distanceRiver)
+
+
+### 3 Chosen model output ################################################################################
+
+m4 <- lmer(sqrt(y) ~ comparison + exposition + PC1soil + PC2soil + PC3soil + side + plotAge + locationYear + 
+             (1|plot), 
+           REML = T,
+           data = tbi)
+
+
+### * Model output ####
+MuMIn::r.squaredGLMM(m4) #R2m = 0.363, R2c = 0.416
+VarCorr(m4)
+sjPlot::plot_model(m4, type = "re", show.values = T)
+car::Anova(m4, type = 2)
+
+### * Effect sizes ####
+(emm <- emmeans(m4, revpairwise ~ comparison, type = "response"))
+plot(emm, comparison = T)
+(emm <- emmeans(m4, revpairwise ~ side, type = "response"))
+
+### * Save ####
+table <- broom::tidy(car::Anova(m4, type = 2))
+write.csv(table, here("outputs/statistics/table_anova_tbi_d_presence.csv"))
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# C Plotten ################################################################################################################
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+library(ggbeeswarm)
+library(ggeffects)
+
+themeMB <- function(){
+  theme(
+    panel.background = element_rect(fill = "white"),
+    text  = element_text(size = 9, color = "black"),
+    axis.text  = element_text(size = 9, color = "black"),
+    legend.text  = element_text(size = 8, color = "black"),
+    strip.text = element_text(size = 9, color = "black"),
+    axis.text.y = element_text(angle = 0, hjust = 0.5),
+    axis.line.y = element_line(),
+    axis.line.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    legend.key = element_rect(fill = "transparent"),
+    legend.justification = c("right", "top"),
+    legend.direction = "vertical",
+    legend.position = c(.95, .45),
+    legend.background = element_rect(fill = "transparent"),
+    legend.margin = margin(0, 0, 0, 0, "cm"),
+    plot.margin = margin(0, 0, 0, 0, "cm")
+  )
+}
+
+pdata <- ggemmeans(m6, terms = c("comparison", "index","exposition"), type = "fe") %>%
+  mutate(group = fct_recode(group, Losses = "B", Gains = "C", Total = "y")) %>%
+  mutate(x = fct_recode(x, "'17 vs. '18" = "1718", "'18 vs. '19" = "1819", "'19 vs. '21" = "1921")) %>%
+  mutate(facet = fct_recode(facet, South = "south", North = "north"))
+pd <- position_dodge(.1)
+(graph <- ggplot(pdata, 
+                 aes(x, predicted, ymin = conf.low, ymax = conf.high, shape = group)) +
+    #geom_quasirandom(tbi = tbi, aes(x, predicted), 
+    #                 color = "grey70", dodge.width = .6, size = 0.7) +
+    geom_errorbar(position = pd, width = 0.0, size = 0.4) +
+    geom_point(position = pd, size = 2.5, fill = "white") +
+    facet_grid(~ facet) +
+    annotate("text", label = "n.s.", x = 2.2, y = 0.6) +
+    scale_y_continuous(limits = c(0, 0.6), breaks = seq(-100, 100, 0.1)) +
+    scale_shape_manual(values = c(25, 24, 16)) +
+    labs(x = "", y = "Temporal beta-diversity", shape = "") +
+    themeMB() +
+    theme()
+)
+
+### Save ###
+ggsave(here("outputs/figures/figure_4_tbi_presence_year_(800dpi_9x5cm).tiff"),
+       dpi = 800, width = 9, height = 5, units = "cm")
+
