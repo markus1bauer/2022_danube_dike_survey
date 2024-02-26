6cba4562863dde59b50cd5a8f10e825872f7d8f2
markus1bauer <markus1.bauer@tum.de>
2024-02-26 16:38:48 +0100

    Hide rgdal

--- R/esy/data/ReadTestdata.R
+++ R/esy/data/ReadTestdata.R
@@ -143,7 +143,7 @@ table(header$Coast_EEA)
 
 ## Add Ecoregion
 # Download shape file from https://storage.googleapis.com/teow2016/Ecoregions2017.zip
-require(rgdal)
+#require(rgdal)
 library(rgeos)
 library(sp)
 ecoreg <- readOGR(dsn = "~/Projekte/GIS/Shapes Global/Ecoregions2017", layer = "Ecoregions2017")
