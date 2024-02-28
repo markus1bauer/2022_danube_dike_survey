1b6a98c782d53de41ea7b8627eedca96e59bf503
markus1bauer <markus1.bauer@tum.de>
2024-02-28 16:57:19 +0100

    Update metadata

--- R/_metadata.R
+++ R/_metadata.R
@@ -1,7 +1,8 @@
 # Beta diversity on dike grasslands
 # Prepare meta data ####
+
 # Markus Bauer
-# 2022-09-15
+# 2024-02-28
 
 
 ### Packages ###
@@ -139,12 +140,14 @@ pub_date <- "2022"
 
 title <- "Beta diversity of restored river dike grasslands is strongly influenced by uncontrolled  spatio-temporal variability"
 
-abstract <- "1.	Spatio-temporal dynamics of biodiversity are a key measure when monitoring restoration success. Balanced species turnover is aimed for because it increases overall biodiversity and improves ecosystem stability and multifunctionality, while nestedness should be avoided, as richness gradients indicate low biodiversity of certain restored sites. For predictive restoration, it is important to analyse beta diversity and to identify its control using site characteristics, spatial effects, historical factors and non-directional year effects.\\n
-2.	We studied dike grasslands 4–19 years after restoration at River Danube in SE Germany over five years (2017–2021, 41 plots in 12 sites). We calculated beta-diversity indices to describe spatial variation and temporal turnover, including their additive components ‘replacement’ and ‘nestedness’, or ‘gains’ and ‘losses’.\\n
-3.	The analysis of the spatial variation of the restored dike grasslands did not reveal homogenisation despite a significant temporal turnover, and was largely dominated by replacement-driven dissimilarity. The drivers of replacement changed over time, although replacement was mainly affected by exposition and spatial factors. Historical factors were inconsistent over time, and no statistically clear drivers were found for nestedness.\\n
-4.	The dike grasslands exhibited on average 37 ± 11% year-to-year turnover in species composition, with some spatio-temporal variation. Gains and losses were balanced over time, although prevalences changed over time and were mostly pronounced on south-exposed slopes.\\n
-5.	In conclusion, the restored grasslands exhibited spatio-temporal turnover controlled by climate and soil variability, varying management regimes, local stochastic (biotic) dynamics and landscape context. Thus, restoration targets defined as a single state should be extended by a desired variation in species composition. Furthermore, the dominance of replacement move the focus from searching the perfect fit for certain restoration targets to a variation of the approaches to increase the beta diversity within a restoration project.\\n
-"
+abstract <- "The data contains vegetation surveys on dike grasslands 4–19 years\\n
+after restoration at River Danube in SE Germany over five years\\n
+(2017–2021, 41 plots in 12 sites, 25 qm per plot).\\n
+The data includes also landscape factors and local soil characteristics and\\n
+histoical factors.\\n
+We calculated beta-diversity indices to describe spatial variation and\\n
+temporal turnover, including their additive components ‘replacement’\\n
+and ‘nestedness’, or ‘gains’ and ‘losses’."
 
 keyword_set <- list(
   list(
@@ -178,11 +181,11 @@ intellectual_rights <- "CC-BY-4.0: https://creativecommons.org/licenses/by/4.0/d
 
 alternate_identifier <- "https://doi.org/10.5281/zenodo.6107806"
 
-short_name <- "Survey of old danube dikes"
+short_name <- "Monitoring of old danube dikes"
 
 language <- "English"
 
-reference_publication <- "Bauer et al. (2023) EcoEvoRxiv DOI XXX"
+reference_publication <- "Bauer et al. (2024) Submitted"
 
 
 
